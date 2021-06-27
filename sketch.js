97c9d4d06f27ee6d2ad48bc72d53de790d24bf31
Ruchi-whitehat <74919297+Ruchi-whitehat@users.noreply.github.com>
2021-06-27 08:04:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const  World = Matter.World;
+const  Bodies = Matter.Bodies;
+
+var snow = [];
+
+function preload() {
+  bgimage = loadImage("snowbackground.jpg");
+
+}
+function setup() {
+  createCanvas(800, 600);
+
+   engine = Engine.create();
+   world = engine.world;
+   ground = new Ground(200, 590, 1200, 30);
+}
+function mousePressed() {
+  
+    
+
+}
+function draw() {
+  background(bgimage);
+   Engine.update(engine);
+   
+   if(frameCount % 20 === 0){
+   snow.push(new SnowFlakes(random(0, 800), 0));
+   }
+    for (var i = 0; i < snow.length; i++) {
+      
+      snow[i].display();
+      
+    }
+  
+  ground.display();
+}
